fbbd48aab381297e6a49ac35e94f04304d9e7797
Serhii Minin <minin.serhii@gmail.com>
2020-03-15 22:08:04 +0200

    allow only GET requests

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,13 @@ const hostname = '127.0.0.1';
 const port = 9000;
 
 const server = http.createServer((req: any, res: any) => {
+  if(req.method !== "GET") {
+    res.statusCode = 405;
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(`The request method ${req.method} is inappropriate for the URL /`);
+  }
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
   res.end('Node http server welcomes you\n');
 });
 
