a4a84b5fbc16bae05c4cb68594fd4e807dd10ef4
Serhii Minin <minin.serhii@gmail.com>
2020-04-03 17:56:45 +0300

    add fetcher

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,17 @@
 import express from "express";
 import React from "react";
 import { renderToString } from "react-dom/server"
+import { StaticRouter } from 'react-router-dom';
 import Index from "../pages"
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/*', async (req, res) => {
+    const app = renderToString(
+      <StaticRouter location={req.url} context={context}>
+          <App />
+      </StaticRouter>
+    );
     const reactComp = renderToString(<Index/>);
     res.status(200).render('pages/index', { reactApp: reactComp });
 });
